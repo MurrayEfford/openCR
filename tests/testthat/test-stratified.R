82791da502164b99ab34b14e28f2cc2d18014d45
MurrayEfford <MurrayEfford@users.noreply.github.com>
2022-01-29 08:41:45 +1300

    test predict() with example stratified fit

--- tests/testthat/test-stratified.R
+++ tests/testthat/test-stratified.R
@@ -13,6 +13,7 @@ suppressWarnings(stratifiedch3 <- stratify (smallCH, covariate = 'Sex'))
 suppressWarnings(ch2 <- subset(ovenCHp, sessions = 1:3, traps = 1:20, dropnullocc = FALSE))
 stratifiedch2 <- stratify (ch2[1:3])
 
+
 test_that("stratify generates suitable data for openCR.fit", {
     ## case 3: split joined data by covariate
     expect_equal(summary(stratifiedch3, terse = TRUE), 
@@ -32,5 +33,24 @@ test_that("correct stratified likelihood", {
         stratified = TRUE)
     expect_equal(do.call(openCR.fit, args)[1], -246.589008072, 
         tolerance = 1e-4, check.attributes = FALSE)
+})
+
+## 2022-01-28 Heiko Hinneberg phidot butterphi
+
+test_that("successful predict() with parameter b and varying sessions per stratum", {
+    # fitting too slow, so pre-fit 2022-01-28
+    # suppressWarnings(ch3 <- subset(ovenCHp, sessions = 1:2, traps = 1:20, dropnullocc = FALSE))
+    # ch2 <- reduce(ch2, by='all') # 3 sessions 
+    # ch3 <- reduce(ch3, by='all') # 2 sessions
+    # stratifiedch4 <- stratify(list(a=ch2, b=ch3))
+    # args <- list(capthist = stratifiedch4, type = 'PLBb',
+    #     start = list(p = 0.4, phi = 0.5, b = 0.1),
+    #     model = p ~ stratum, details = list(LLonly = FALSE),
+    #     stratified = TRUE, trace=T)
+    # stratifiedfit <- do.call(openCR.fit, args)
+    # saveRDS(stratifiedfit, file = 'd:/open populations/openCR/inst/exampledata/stratifiedfit.RDS')
+    
+    stratifiedfit <- readRDS(system.file("exampledata", "stratifiedfit.RDS", package = "openCR"))
+    expect_silent(pred <- predict(stratifiedfit))
 })
 
